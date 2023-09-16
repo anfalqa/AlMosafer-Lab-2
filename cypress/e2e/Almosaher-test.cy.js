6461a2d6fb17a94acee8aafd60492d44b611748e
anfalqa <anfalaldraidi2001@gmail.com>
2023-09-16 11:22:21 +0300

    add the test

--- cypress/e2e/Almosaher-test.cy.js
+++ cypress/e2e/Almosaher-test.cy.js
@@ -1,5 +1,9 @@
 ///<reference types= "cypress" />
 
+Cypress.on('uncaught:exception', err => {
+    return false
+});
+let lower=0;
 let randomArrEn = Math.floor(Math.random() * 3);
 let arrEn = ['Dubai', 'Jeddah', 'Amman']
 describe('AlMosafer-Lab 2', () => {
@@ -9,17 +13,21 @@ describe('AlMosafer-Lab 2', () => {
     cy.get('#uncontrolled-tab-example-tab-hotels').click()
     cy.get('[data-testid="AutoCompleteInput"]').type(`${arrEn[randomArrEn]}`)
     cy.get('[data-testid="HotelSearchBox__SearchButton"]').click()
-    cy.wait(20000)
+    cy.wait(18000)
     cy.get('[data-testid="HotelSearchResult__sort__LOWEST_PRICE"]').click()
-    cy.wait(20000)
+    cy.wait(15000)
     
-    cy.get('[data-testid="HotelSearchResult__Hotel0__PriceLabel"] > .Price__Value').as(lowerValeu)
-    cy.get('[data-testid="HotelSearchResult__Hotel39__PriceLabel"] > .Price__Value').as(greaterValeu)
+    cy.get('[data-testid="HotelSearchResult__Hotel0__PriceLabel"] > .Price__Value').invoke('text').then(text =>{
+      lower=Number(text)
+    })
+    cy.get('[data-testid="HotelSearchResult__Hotel39__PriceLabel"] > .Price__Value').invoke('text').then(text => {
+      let greater = Number(text);
+      cy.wrap(`${greater}`).should('be.greaterThan', cy.wrap(`${Number(lower)}`));
+    });
 
 
-
-
-
+  })
+})
 
    // let lower= Math.floor(cy.get('[data-testid="HotelSearchResult__Hotel0__PriceLabel"] > .Price__Value'))
    // let greater = Math.floor(cy.get('[data-testid="HotelSearchResult__Hotel39__PriceLabel"] > .Price__Value'))
@@ -31,7 +39,3 @@ describe('AlMosafer-Lab 2', () => {
  // }
 
   
-
-
-  })
-})
