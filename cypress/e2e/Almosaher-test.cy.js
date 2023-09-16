8f3ec86089a5dc3e14f21619060642123a3a549c
anfalqa <anfalaldraidi2001@gmail.com>
2023-09-16 12:42:32 +0300

    add the test

--- cypress/e2e/Almosaher-test.cy.js
+++ cypress/e2e/Almosaher-test.cy.js
@@ -1,9 +1,9 @@
 ///<reference types= "cypress" />
 
 Cypress.on('uncaught:exception', err => {
-    return false
+  return false
 });
-let lower=0;
+let lower = 0;
 let randomArrEn = Math.floor(Math.random() * 3);
 let arrEn = ['Dubai', 'Jeddah', 'Amman']
 describe('AlMosafer-Lab 2', () => {
@@ -15,27 +15,27 @@ describe('AlMosafer-Lab 2', () => {
     cy.get('[data-testid="HotelSearchBox__SearchButton"]').click()
     cy.wait(18000)
     cy.get('[data-testid="HotelSearchResult__sort__LOWEST_PRICE"]').click()
-    cy.wait(15000)
-    
-    cy.get('[data-testid="HotelSearchResult__Hotel0__PriceLabel"] > .Price__Value').invoke('text').then(text =>{
-      lower=Number(text)
-    })
-    cy.get('[data-testid="HotelSearchResult__Hotel39__PriceLabel"] > .Price__Value').invoke('text').then(text => {
+    cy.wait(5000)
+   
+    cy.get('[data-testid="HotelSearchResult__Hotel0__PriceLabel"] > .Price__Value').invoke('text').then(text => {
+      lower = Number(text);
+      cy.log(lower)
+      cy.get('[data-testid="HotelSearchResult__Hotel39__PriceLabel"] > .Price__Value').invoke('text').then(text => {
       let greater = Number(text);
-      cy.wrap(`${greater}`).should('be.greaterThan', cy.wrap(`${Number(lower)}`));
+      expect(lower).to.be.lessThan(greater)
     });
-
+  })
 
   })
 })
 
-   // let lower= Math.floor(cy.get('[data-testid="HotelSearchResult__Hotel0__PriceLabel"] > .Price__Value'))
-   // let greater = Math.floor(cy.get('[data-testid="HotelSearchResult__Hotel39__PriceLabel"] > .Price__Value'))
+// let lower= Math.floor(cy.get('[data-testid="HotelSearchResult__Hotel0__PriceLabel"] > .Price__Value'))
+// let greater = Math.floor(cy.get('[data-testid="HotelSearchResult__Hotel39__PriceLabel"] > .Price__Value'))
 //if (greater < lower){
-   // alert("HERE WE HAVE ERORR! THE LOWEST PRICE GREATER THAN THE GREATEST IN THIS PAGE!")
-  // }
+// alert("HERE WE HAVE ERORR! THE LOWEST PRICE GREATER THAN THE GREATEST IN THIS PAGE!")
+// }
 //else{
-   // alert("THE TEST IS PASS !")
- // }
+// alert("THE TEST IS PASS !")
+// }
 
-  
+
